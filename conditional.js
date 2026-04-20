b10e20b552d1b0699c523baa2e37199bb0ea3a4e
Aunik sachan-abesec <auniksach12@gmail.com>
2026-04-20 21:24:41 +0530

    Conditional statement in JS

---
+++ conditional.js
@@ -0,0 +1,23 @@
+let age=20;
+if(age>=18){
+    console.log("You are adult");
+}
+else{
+    console.log("You are a minor");
+} 
+
+
+var garde="A";
+document.write("Entering switch block<br/>");
+switch(grade){
+    case "A":document.write("A grade");
+    break;
+    case "B":document.write("B grade");
+    break;
+    case "C":document.write("C grade");
+    break;
+    case "D":document.write("D grade");
+    break;
+    default:document.write("Wrong grade");
+}
+document.write("Exiting switch block");
