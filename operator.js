99b586d37c2322b5be8e121867e8ff607f2b3aaf
Aunik sachan-abesec <auniksach12@gmail.com>
2026-04-20 21:18:28 +0530

    Operator(JS)

---
+++ operator.js
@@ -0,0 +1,31 @@
+var a=20;
+var b=10;
+
+// Arithmetic operators
+console.log("a + b = "+(a+b));
+console.log("a - b = "+(a-b));
+console.log("a * b = "+(a*b));
+console.log("a / b = "+(a/b));
+console.log("a % b = "+(a%b));
+console.log("a ** b = "+(a**b));
+console.log("a++ = "+(a++));
+console.log("a-- = "+(a--));
+
+
+// Comparison operators
+console.log("a > b = "+(a>b));
+console.log("a < b = "+(a<b));
+console.log("a >= b = "+(a>=b));
+console.log("a <= b = "+(a<=b));
+console.log("a != b = "+(a!=b));
+console.log("a == b = "+(a==b));
+console.log("a === b = "+(a===b));  //Strict equal (value + type)
+
+// Logical operators
+console.log("(a>10 && b>20) = "+(a>10 && b>20));
+console.log("(a>10 || b>20) = "+(a>10 || b>20));
+console.log("(!(a>10)) = "+!(a>10));
+
+// Ternary operator
+console.log("Condition?True:False;"+(a==b?true:false));
+
