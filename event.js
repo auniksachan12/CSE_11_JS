f0b8daf2955d71f6913fba4a94e8b4b871b4d528
Aunik sachan-abesec <auniksach12@gmail.com>
2026-04-20 21:23:03 +0530

    Event listener in JS

---
+++ event.js
@@ -0,0 +1,33 @@
+// // mouse events-onclick,onmouseover,onmouseout,onmousedown,onmouseup,onmousemove,ondoubleclick
+function onClick(){
+    console.log("clicked");
+}
+function onMouseOver(){
+    console.log("on mouse over");
+}
+function onMouseOut(){
+    console.log("on mouse out");
+}
+function onMouseDown(){
+    console.log("on mouse down");
+}
+function onMouseUp(){
+    console.log("on mouse up");
+}
+function onMouseMove(){
+    console.log("on mouse move");
+}
+function onDoubleClick(){
+    console.log("on double click");
+}
+
+
+
+// // keyboard events-on key up,on key down
+// // form events-on focus,on change,on blur,on submit
+// // addEventListener('focus',onhandlefocus);
+// // window events-onload,onunload,onresize
+function mybody(){
+    alert("Window Loaded successfully")
+}
+
